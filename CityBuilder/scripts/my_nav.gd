04bde3b72076095979dfa7d4dbdb32bf213c9151
BeesAreMyThing <kdkatz888@gmail.com>
2017-12-10 16:58:51 -0500

    Constrained camera panning

--- CityBuilder/scripts/my_nav.gd
+++ CityBuilder/scripts/my_nav.gd
@@ -4,30 +4,32 @@ extends Camera2D
 # var a = 2
 # var b = "textvar"
 
-var max_x = 50 # in tiles
-var max_y = 25 # in tiles
-var pan_active = true
+var map_edges = {"x":{"max":0, "min":0}, "y":{"max":0, "min":0}}
+var pan_active = false
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	
 	print("camera rdy")
 	set_process(true) #player interactions
 	set_process_input(true) #also player interactions
-	self.set_pos(Vector2(0,0))
 	pass
 
 func constrained_pos():
+	var x_offset = 500 
+	var y_offset = 500 
 	#Get the mouse position, but restict it to the map size
 	var pos = get_global_mouse_pos()
-	if pos.x < 0:
-		pos.x = 0
-	if pos.y < 0:
-		pos.y = 0
-	if pos.x > (max_x * 32):
-		pos.x = (max_x * 32)
-	if pos.y > (max_y * 32):
-		pos.y = (max_y * 32)
+	
+	if pos.x < map_edges["x"]["min"]:
+		pos.x = map_edges["x"]["min"]
+	if pos.y < map_edges["y"]["min"]:
+		pos.y = map_edges["y"]["min"]
+	if pos.x > map_edges["x"]["max"] - x_offset:
+		pos.x = map_edges["x"]["max"] - x_offset
+	if pos.y > map_edges["y"]["max"] - y_offset:
+		pos.y = map_edges["y"]["max"] - y_offset
 		
 	return pos
 
--- CityBuilder/scripts/tilePlacer.gd
+++ CityBuilder/scripts/tilePlacer.gd
@@ -6,6 +6,7 @@ const Building = preload("res://scripts/buildings/building.gd") # Relative path
 # var a = 2
 # var b = "textvar"
 var grid = {}
+var map_edges = {"x":{"max":0, "min":0}, "y":{"max":0, "min":0}}
 var color = Color(.5,0,.5)
 var line = 3
 var halfTileSize = Vector2(16, 16)
@@ -14,21 +15,38 @@ var tileset = load("res://resources/land_tiles.tres")
 var hover = {"object":null, "highlight":null}
 
 func get_tile_at(pos):
-	return grid[snap_to_tile(pos)]
+	var tile_pos = snap_to_tile(pos)
+	print(tile_pos)
+	if grid.has(tile_pos):
+		print(grid[snap_to_tile(pos)])
+		return grid[snap_to_tile(pos)]
+	else:
+		print("None")
+		return ""
 
 func change_tile_at(pos, newValue):
 	grid[snap_to_tile(pos)] = newValue
 
 	 
+func find_edges():
+	for pos in grid.keys():
+		if pos.x < map_edges["x"]["min"]:
+			map_edges["x"]["min"] = pos.x 
+		if pos.x > map_edges["x"]["max"]:
+			map_edges["x"]["max"] = pos.x 
+		if pos.y < map_edges["y"]["min"]:
+			map_edges["y"]["min"] = pos.y 
+		if pos.y > map_edges["y"]["max"]:
+			map_edges["y"]["max"] = pos.y
+	get_child(0).set("map_edges", map_edges) 
+	print(map_edges)
 
 func initialize_grid():
 	#get used cells into an array (not real world pos)
-	var tiles = get_used_cells()
-	
 	#get cell world pos, centralize it and append to grid array
-	for pos in tiles:
+	for pos in get_used_cells():
 		var tile_name = tileset.tile_get_name(get_cell(pos.x,pos.y))
-		var global_pos = map_to_world(pos) + halfTileSize
+		var global_pos = map_to_world(pos + halfTileSize)
 		grid[global_pos] = tile_name
 
 func new_building_at(name, pos, alpha=1):
@@ -49,10 +67,11 @@ func get_tile_texture(name):
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	
 	set_process(true) #player interactions
 	set_process_input(true) #also player interactions
 	initialize_grid()
-	pass
+	find_edges()
 
 func _process(delta):
 	pass
@@ -70,7 +89,7 @@ func _input(event):
 	
 	if hover["object"] != null:
 		hover["object"].set_pos(snap_to_tile(get_global_mouse_pos()))
-		print(get_tile_at(get_global_mouse_pos()))
+		get_tile_at(get_global_mouse_pos())
 	if hover["object"] != null and event.is_action_released("mouse_act_right"):
 		hover["object"].set_opacity(1)
 		hover["object"] = null
